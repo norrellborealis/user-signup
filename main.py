26607c71e00b19272b5a0ad4544d6c1fecbf813e
norrellborealis <tkkrings425@gmail.com>
2017-10-03 23:15:47 -0500

    mostly working

---
+++ main.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    return render_template('signup_form.html')
+
+@app.route("/validate", methods=['POST'])
+def validate():
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify_password']
+    email = request.form['email']
+    
+    error_check = False
+    username_err = ''
+    password_err = ''
+    verify_password_error = ''
+    email_err = ''
+
+    if " " in username or username == '':
+        username_err = "Not a proper username."
+        error_check = True
+    elif len(username) < 3 or len(username) > 20:
+        username_err = "Username is not the proper length."
+        error_check = True
+    if ' ' in password or password == '':
+        password_err = "Not a proper password."
+        password = ''
+        verify_password = ''
+        error_check = True
+    elif len(password) < 3 or len(password) > 20:
+        password_err = "Password is not the proper length."
+        password = ''
+        verify_password = ''
+        error_check = True
+    if password != verify_password:
+        verify_password_error = "Passwords don't match."
+        password = ''
+        verify_password = ''
+        error_check = True
+    elif verify_password == '':
+        verify_password_error = "Passwords don't match."
+        error_check = True
+    
+    if email != '':
+        if email.count('@') != 1:
+            email_err = "Not a valid email address."
+            error_check = True
+        if email.count('.') != 1:
+            email_err = "Not a valid email address."
+            error_check = True
+        if " " in email:
+            email_err = "Not a valid email address."
+            error_check = True
+        if len(email[0:email.find('@')]) < 3 or len(email[0:email.find('@')]) > 20:
+            email_err = "Not a valid email address."
+            error_check = True
+    if error_check == True:
+        return render_template("signup_form.html", username_err=username_err,
+            password_err=password_err, verify_password_error=verify_password_error,
+            email_err=email_err, username=username, password=password,
+            verify_password=verify_password, email=email)
+    else:
+        return render_template("welcome_greeting.html", username=username)
+app.run()
